75d7a5d0159d08ac103cdd03103126f5405d9429
PGM15 <gonzalezmediavillapablo@gmail.com>
2025-12-10 18:32:10 +0100

    Cambios en la UX

--- proyecto-final/src/app/layout.js
+++ proyecto-final/src/app/layout.js
@@ -2,16 +2,17 @@ import "./globals.css";
 import { FavoriteProvider } from "../context/FavoriteContext";
 
 export const metadata = {
-  title: "Spotify Playlist Generator",
+  title: "Spotify Taste Mixer",
   description: "Proyecto Final Web",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="min-h-screen flex justify-center">
+      <body className="w-full min-h-screen overflow-x-hidden">
         <FavoriteProvider>
-          <main className="w-full max-w-6xl px-4 sm:px-6 lg:px-10">
+          {/* Contenedor sin max-width para permitir fondo completo */}
+          <main className="w-full min-h-screen">
             {children}
           </main>
         </FavoriteProvider>
